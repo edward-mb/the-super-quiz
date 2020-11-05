9b6141a56b363ab921ff491410f730f9f3123fb4
José Eduardo Méndez Barrios <joseeduardomendezb@gmail.com>
2020-11-05 04:44:48 -0700

    The Super Quiz initial base code

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,14 @@
     <div class="columns is-mobile is-multiline is-centered">
       <div class="column is-two-thirds has-text-centered">
         <img
-          class="image is-128x128 is-inline-block"
+          class="image is-64x64 is-inline-block"
           alt="Vue logo"
           src="./assets/logo.png"
         />
       </div>
       <div class="column is-two-thirds">
-        <h1 class="is-size-1">Animations</h1>
+        <h1 class="is-size-1">The Super Quiz</h1>
         <hr />
-        <button class="button is-primary" @click="show = !show">
-          Show Alert
-        </button>
-        <br />
-        <br />
-        <transition>
-          <div class="notification is-info" v-if="show">This is some info</div>
-        </transition>
       </div>
     </div>
   </div>
